5949f7d0719b4b7d84880b39c0d049515be42607
Kirabo19 <kapojames@gmail.com>
2022-12-14 23:57:55 +0300

    Add and remove reducers

--- src/components/book.js
+++ src/components/book.js
@@ -2,24 +2,43 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Book = (props) => {
-  const { title, author } = props;
+  const {
+    category, click, id, title, author,
+  } = props;
 
   return (
     <div>
-      <li>
-        {title}
-        {' '}
-        |
-        {author}
-      </li>
-
+      <p>{category}</p>
+      <h2>{title}</h2>
+      <p>{author}</p>
+      <ul>
+        <li>
+          <button type="button">Comments</button>
+        </li>
+        <li>
+          <button id={id} type="button" onClick={click}>Remove</button>
+        </li>
+        <li>
+          <button type="button">Edit</button>
+        </li>
+      </ul>
+      <div>64% Completed</div>
+      <div>
+        <p>CURRENT CHAPTER</p>
+        <p>Chapter 17</p>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
     </div>
   );
 };
+
 Book.propTypes = {
 
+  category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  click: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
 
 };
 
--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,27 +1,42 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux/';
 import Book from './book';
 import Form from './form';
+import { removeBook } from '../redux/books/books';
 
 const BooksList = () => {
-  const bookList = [
-    {
-      id: 1,
-      title: 'The way of Kings',
-      author: 'Brandon',
-    },
-    {
-      id: 2,
-      title: 'The dragon reborn',
-      author: 'Marcus Rufus',
-    },
-  ];
-  const list = bookList.map((books) => (
+  const dispatch = useDispatch();
 
-    <Book
-      key={books.id}
-      title={books.title}
-      author={books.author}
-    />
+  const bookList = useSelector((state) => state.booksReducer);
+
+  const click = (e) => {
+    dispatch(removeBook(e.target.id));
+  };
+
+  // const bookList = [
+  //   {
+  //     id: 1,
+  //     title: 'The way of Kings',
+  //     author: 'Brandon',
+  //     category: 'Kings'
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'The dragon reborn',
+  //     author: 'Marcus Rufus',
+  //     category: 'Romans'
+  //   },
+  // ];
+  const list = bookList.map((books) => (
+    <li key={books.id}>
+      <Book
+        id={books.id}
+        click={click}
+        title={books.title}
+        author={books.author}
+        category={books.category}
+      />
+    </li>
   ));
 
   return (
