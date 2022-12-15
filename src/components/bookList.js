c639c3d4f2cf84ed42ba34c6e4466c1083fa579d
Kirabo19 <kapojames@gmail.com>
2022-12-15 12:38:50 +0300

    Call API

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux/es/exports';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './book';
-import Form from './form';
-import { removeBook } from '../redux/books/books';
+import { getBooks, removeBookReducer } from '../redux/books/books';
 
 const BooksList = () => {
   const dispatch = useDispatch();
 
-  const bookList = useSelector((state) => state.booksReducer);
+  const bookInfo = useSelector((state) => state.booksReducer);
 
   const click = (e) => {
-    dispatch(removeBook(e.target.id));
+    dispatch(removeBookReducer(e.target.id));
   };
 
-  const list = bookList.map((books) => (
-    <li key={books.id}>
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
+
+  const list = bookInfo.map((item) => (
+    <li key={item.id}>
       <Book
-        id={books.id}
+        id={item.id}
         click={click}
-        title={books.title}
-        author={books.author}
-        category={books.category}
+        title={item.title}
+        category={item.category}
       />
     </li>
   ));
   return (
-    <div>
-      <ul>
-        { list }
-      </ul>
-      <Form />
-    </div>
+    <ul>
+      {list}
+    </ul>
   );
 };
 export default BooksList;
